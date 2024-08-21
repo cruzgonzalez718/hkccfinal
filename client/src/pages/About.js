5d06f6844d504f6ef9cbda758be34c62a39442e7
dsfawsd <96902481+VictorNiu1@users.noreply.github.com>
2024-08-20 20:22:25 -0700

    About page styling

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import './Projects.css';
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>Learn more about us on this page.</p>
+    <div className="projects-container">
+      <div style={{ padding: '40px', textAlign: 'center' }}>
+        <h2 style={{ color: '#3375ff ' }}  >At Hope Kids Cyber Classroom (HopeKCC), inspired by Christian values, we are committed to creating a dynamic learning community where children (Kids), college and graduate students (Candidates), and experienced professionals (Counselors) come together to empower and support one another through technology-enabled education.</h2>
+        <h2 style={{ color: '#3375ff ' }}  >Drawing parallels to the mission of organizations like YMCA, we infuse our programs with a unique tech twist, offering innovative learning experiences that nurture the mind, body, and spirit. Our dedicated counselors provide insightful tech-talk sessions to our Candidates, equipping them with the knowledge and skills needed to thrive in today’s digital landscape. Meanwhile, our Kids group benefits from engaging classes delivered by our counselors, enhanced by the active involvement of our Candidates who serve as coaches for each class, providing additional support and guidance to enrich the learning experience.</h2>
+        <h2 style={{ color: '#3375ff ' }}  >Additionally, our Candidates play a vital role in our mission by offering mentorship to our Kids group, guiding and inspiring them to reach their full potential within a supportive community.</h2>
+        <h2 style={{ color: '#3375ff ' }}  >At HopeKCC, we believe in the transformative power of education and the importance of fostering connections across generations. Rooted in Christian principles and guided by our mission to serve, we strive to create a brighter future for all through innovative learning opportunities and collaborative community engagement.</h2>
+      </div>
     </div>
   );
 };
